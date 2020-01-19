5b4c776b03c848118bc77b27dedfb101003bfa52
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-18 20:41:22 -0800

    creating connection to get url from an api

--- src/main/java/quotes/App.java
+++ src/main/java/quotes/App.java
@@ -6,22 +6,42 @@ package quotes;
 
 import com.google.gson.Gson;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        Gson gson = new Gson();
 
-        Quote[] quotes = gson.fromJson(new FileReader("src/main/resources/quotes.json"),Quote[].class);
-        Quote random = quotes[randomizeQuotes(quotes.length)];
-        System.out.println(random);
+//        Reading stuff from files
+        try {
+            getRequest();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+//            internet fails
+
+            Gson gson = new Gson();
+
+            Quote[] quotes = gson.fromJson(new FileReader("src/main/resources/quotes.json"), Quote[].class);
+            Quote random = quotes[randomizeQuotes(quotes.length)];
+            System.out.println(random);
+        }
     }
 
     public static int randomizeQuotes(int max) {
-        int x = (int)(Math.random() * max);
+        int x = (int) (Math.random() * max);
         return x;
     }
 
+    public static void getRequest() throws IOException {
+
+        URL url = new URL("https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote");
+        HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+        connect.setRequestMethod("GET");
+
+        String data = new BufferedReader(new InputStreamReader(connect.getInputStream())).readLine();
+        System.out.println(data);
+    }
 }
 
